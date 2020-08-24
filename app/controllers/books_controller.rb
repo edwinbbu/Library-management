817931f0b116467b1247392f7c0e36d8e4a77b97
Edwin Babu <edwinbbu@gmail.com>
2020-08-24 10:45:00 +0530

    tailwind added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,7 +25,7 @@ class BooksController < ApplicationController
   # POST /books.json
   def create
     @book = Book.new(book_params)
-
+    @departments = Department.all()
     respond_to do |format|
       if @book.save
         format.html { redirect_to @book, notice: 'Book was successfully created.' }
